e4f703756d2d655a7a3b8cc57a3bd8d1cbf92b33
huangwei200012 <155598454+huangwei200012@users.noreply.github.com>
2025-05-22 15:45:40 +0800

    Update quiet_star_mistral.sh

--- quiet_star_mistral.sh
+++ quiet_star_mistral.sh
@@ -1,11 +1,11 @@
 n_ahead_talk_global=8
 n_ahead_global=16
-model_name=/nlp_group/decapoda-research/Mistral-7B-v0.1
+model_name=Mistral-7B-v0.1
 mode="base"
 lr=2e-6
-output_dir=/mmu_nlp_hdd/huangwei12/research/quiet-star-new/outputs/mistral/${n_ahead_global}_${n_ahead_talk_global}_${mode}_${lr}
+output_dir=fast-quiet-star/outputs/mistral/${n_ahead_global}_${n_ahead_talk_global}_${mode}_${lr}
 mkdir -p $output_dir
-python -u /mmu_nlp_hdd/huangwei12/research/quiet-star-new/quiet-star-train.py \
+python -u quiet-star-train.py \
     --n_ahead_talk_global $n_ahead_talk_global \
     --n_ahead_global $n_ahead_global \
     --mode $mode \
@@ -13,40 +13,42 @@ python -u /mmu_nlp_hdd/huangwei12/research/quiet-star-new/quiet-star-train.py \
     --model_name ${model_name} \
     --lr $lr > $output_dir/train_log 2>&1
 
-bash /mmu_nlp_hdd/huangwei12/research/quiet-star-new/run_sh/eval.sh ${output_dir} ${n_ahead_global}
+bash fast-quiet-star/run_sh/eval.sh ${output_dir} ${n_ahead_global}
 
 
 n_ahead_talk_global=4
 n_ahead_global=12
+model_name=16_8_CKPT
 mode="base"
 lr=1e-6
-output_dir=/mmu_nlp_hdd/huangwei12/research/quiet-star-new/outputs/mistral/${n_ahead_global}_${n_ahead_talk_global}_${mode}_${lr}
+output_dir=fast-quiet-star/outputs/mistral/${n_ahead_global}_${n_ahead_talk_global}_${mode}_${lr}
 mkdir -p $output_dir
-python -u /mmu_nlp_hdd/huangwei12/research/quiet-star-new/quiet-star-train.py \
+python -u quiet-star-train.py \
     --n_ahead_talk_global $n_ahead_talk_global \
     --n_ahead_global $n_ahead_global \
     --mode $mode \
     --output_dir $output_dir \
-    --model_name /nlp_group/decapoda-research/Mistral-7B-v0.1 \
+    --model_name ${model_name} \
     --lr $lr > $output_dir/train_log 2>&1
 
-bash /mmu_nlp_hdd/huangwei12/research/quiet-star-new/run_sh/eval.sh ${output_dir} ${n_ahead_global}
+bash fast-quiet-star/run_sh/eval.sh ${output_dir} ${n_ahead_global}
 
 
 
 n_ahead_talk_global=4
 n_ahead_global=8
+model_name=12_4_CKPT
 mode="base"
 lr=1e-6
-output_dir=/mmu_nlp_hdd/huangwei12/research/quiet-star-new/outputs/mistral/${n_ahead_global}_${n_ahead_talk_global}_${mode}_${lr}
+output_dir=fast-quiet-star/outputs/mistral/${n_ahead_global}_${n_ahead_talk_global}_${mode}_${lr}
 mkdir -p $output_dir
-python -u /mmu_nlp_hdd/huangwei12/research/quiet-star-new/quiet-star-train.py \
+python -u quiet-star-train.py \
     --n_ahead_talk_global $n_ahead_talk_global \
     --n_ahead_global $n_ahead_global \
     --mode $mode \
     --output_dir $output_dir \
-    --model_name /nlp_group/decapoda-research/Mistral-7B-v0.1 \
+    --model_name ${model_name} \
     --lr $lr > $output_dir/train_log 2>&1
 
-bash /mmu_nlp_hdd/huangwei12/research/quiet-star-new/run_sh/eval.sh ${output_dir} ${n_ahead_global}
+bash fast-quiet-star/run_sh/eval.sh ${output_dir} ${n_ahead_global}
 
