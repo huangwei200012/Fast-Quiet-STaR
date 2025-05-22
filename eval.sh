bf5c2d8021de067915d33a0c6342a89271dccb0e
huangwei200012 <155598454+huangwei200012@users.noreply.github.com>
2025-05-22 15:49:21 +0800

    Create eval.sh

---
+++ eval.sh
@@ -0,0 +1,28 @@
+
+
+
+base_path=$1
+n_ahead=$2
+ckpts=(10 20 30 40 50 60 70 80)
+num_ckpts=${#ckpts[@]}
+GPUs="0"
+for ((i=0; i<$num_ckpts; i++)); do
+    ckpt=${ckpts[$i]}
+    CUDA_VISIBLE_DEVICES=${GPUs} python -u quiet-star-eval.py \
+        --n_ahead ${n_ahead} \
+        --model_name $base_path/checkpoint-${ckpt} > $base_path/checkpoint_${ckpt}_eval_log 2>&1 &
+    GPUs=$(($GPUs+1))
+done
+wait
+
+ckpts=(90 100 110 120 130 140 150)
+num_ckpts=${#ckpts[@]}
+GPUs="0"
+for ((i=0; i<$num_ckpts; i++)); do
+    ckpt=${ckpts[$i]}
+    CUDA_VISIBLE_DEVICES=${GPUs} python -u quiet-star-eval.py \
+        --n_ahead ${n_ahead} \
+        --model_name $base_path/checkpoint-${ckpt} > $base_path/checkpoint_${ckpt}_eval_log 2>&1 &
+    GPUs=$(($GPUs+1))
+done
+wait
