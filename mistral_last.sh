6f561e30130fc8cc72cd22fd32239ff0506151bf
huangwei200012 <155598454+huangwei200012@users.noreply.github.com>
2025-05-22 15:36:24 +0800

    Create mistral_last.sh

---
+++ mistral_last.sh
@@ -0,0 +1,21 @@
+n_ahead_talk_global=1
+n_ahead_global=1
+mode="last_rl"
+lr=1e-6
+data_path=/mmu_nlp_hdd/huangwei12/research/quiet-star-new/quiet_infer/mistral/12_4_base_1e-6-16_8_50_base_30
+data_name=12_4_base_1e-6-16_8_50_base_30
+model_name=mistral_base
+
+output_dir=/mmu_nlp_hdd/huangwei12/research/quiet-star-new/outputs/mistral/${n_ahead_global}_${n_ahead_talk_global}_${mode}_${lr}_${model_name}_${data_name}
+mkdir -p $output_dir
+python -u /mmu_nlp_hdd/huangwei12/research/quiet-star-new/quiet-star-train.py \
+    --n_ahead_talk_global $n_ahead_talk_global \
+    --n_ahead_global $n_ahead_global \
+    --mode $mode \
+    --data_path $data_path \
+    --output_dir $output_dir \
+    --model_name /nlp_group/decapoda-research/Mistral-7B-v0.1 \
+    --lr $lr > $output_dir/train_log 2>&1
+bash /mmu_nlp_hdd/huangwei12/research/quiet-star-new/run_sh/eval_last.sh $output_dir 1
+
+
